84d1cff263b6c8307af77ea76a8c2b637bde9602
9aoyang <9aoyang@gmail.com>
2017-11-08 15:49:02 +0800

    learn redux

--- src/index.js
+++ src/index.js
@@ -2,62 +2,87 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const appState = {
-    title: {
-        text: 'React.js 小书',
-        color: 'red',
-    },
-    content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-    }
-};
-
-function createStore(state, _stateChanger) {
+function createStore(reducer) {
+    let state = null;
     const listeners = [];
     const subscribe = (listener) => listeners.push(listener);
     const getState = () => state;
     const dispatch = (action) => {
-        stateChanger(state, action);
+        state = reducer(state, action);
         listeners.forEach((listener) => listener());
     };
+    dispatch({}); // 初始化 state
     return { getState, dispatch, subscribe };
 }
 
-function renderApp(State) {
-    renderTitle(State.title);
-    renderContent(State.content);
+function renderApp(newAppState, oldAppState = {}) { // 防止 oldAppState 没有传入，所以加了默认参数 oldAppState = {}
+    if (newAppState === oldAppState) return; // 数据没有变化就不渲染了
+    console.log('render app...');
+    renderTitle(newAppState.title, oldAppState.title);
+    renderContent(newAppState.content, oldAppState.content);
 }
 
-function renderTitle(title) {
+function renderTitle(newTitle, oldTitle = {}) {
+    if (newTitle === oldTitle) return; // 数据没有变化就不渲染了
+    console.log('render title...');
     const titleDOM = document.getElementById('title');
-    titleDOM.innerHTML = title.text;
-    titleDOM.style.color = title.color;
+    titleDOM.innerHTML = newTitle.text;
+    titleDOM.style.color = newTitle.color;
 }
 
-function renderContent(content) {
+function renderContent(newContent, oldContent = {}) {
+    if (newContent === oldContent) return; // 数据没有变化就不渲染了
+    console.log('render content...');
     const contentDOM = document.getElementById('content');
-    contentDOM.innerHTML = content.text;
-    contentDOM.style.color = content.color;
+    contentDOM.innerHTML = newContent.text;
+    contentDOM.style.color = newContent.color;
 }
 
-function stateChanger(state, action) {
+function reducer(state, action) {
+    if (!state) {
+        return {
+            title: {
+                text: 'React.js 小书',
+                color: 'red',
+            },
+            content: {
+                text: 'React.js 小书内容',
+                color: 'blue'
+            }
+        };
+    }
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text;
-            break;
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            };
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color;
-            break;
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            };
         default:
-            break;
+            return state;
     }
 }
 
-const store = createStore(appState, stateChanger);
-store.subscribe(() => renderApp(store.getState())); // 监听数据变化
+const store = createStore(reducer);
+let oldState = store.getState(); // 缓存旧的 state
+store.subscribe(() => {
+    const newState = store.getState(); // 数据可能变化，获取新的 state
+    renderApp(newState, oldState); // 把新旧的 state 传进去渲染
+    oldState = newState; // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+});
+
+
 
 renderApp(store.getState()); // 首次渲染页面
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小🐶》' }); // 修改标题文本
 store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'green' }); // 修改标题颜色
-
